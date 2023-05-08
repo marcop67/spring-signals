96c255bf712c2fa1fe41028a20480aeeaceb254c
Kalpa D. Fernando <50633428+KalpaD98@users.noreply.github.com>
2023-05-08 16:48:47 +0530

    removed unwanted import

--- src/main/java/com/copytrading/bot/controller/TutorialController.java
+++ src/main/java/com/copytrading/bot/controller/TutorialController.java
@@ -2,7 +2,6 @@ package com.copytrading.bot.controller;
 
 import com.copytrading.bot.model.Bybit;
 import com.squareup.okhttp.ResponseBody;
-import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
